3007c17449ca2a966f43ddd3179af243dae3d3fb
Mert Saygı <mertsaygi@gmail.com>
2016-01-09 22:22:34 +0200

    Making some improvements on progress bar

--- app/src/main/java/net/mertsaygi/heartprogress/HeartProgressBar.java
+++ app/src/main/java/net/mertsaygi/heartprogress/HeartProgressBar.java
@@ -6,10 +6,10 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PathMeasure;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.RelativeLayout;
 
 /**
  * Created by mertsaygi on 04/01/16.
@@ -20,9 +20,12 @@ public class HeartProgressBar extends View {
     private Paint paint;
     private Path path;
     private float firstRoundWidth = 15;
-    private float secondRoundWidth = 5;
+    private float secondRoundWidth = 10;
     private int backColor = Color.RED;
     private int progressColor = Color.YELLOW;
+    private int w;
+    private int h;
+    private Rect rectf;
 
     public HeartProgressBar(Context context) {
         super(context);
@@ -36,15 +39,28 @@ public class HeartProgressBar extends View {
         super(context, attrs, defStyleAttr);
     }
 
-    private Path firstPart(Canvas canvas,float length,float x,float y){
+    private Path firstPart(Canvas canvas,float x,float y){
         path = new Path();
 
         canvas.rotate(45, x, y);
 
-        path.moveTo(x, y);
-        path.lineTo(x - length, y);
-        path.arcTo(new RectF(x - length - (length / 2), y - length, x - (length / 2), y), 90, 180);
-        path.arcTo(new RectF(x - length, y - length - (length / 2), x, y - (length / 2)), 180, 180);
+        float l1 = y/2;
+        float l2 = y/2;
+        path.moveTo(x - l1, y);
+        path.lineTo(x - l1, y);
+
+        float left =  x - l1 - (l1 / 2);
+        float top =  y - l1;
+        float right = x - (l1 / 2);
+        float bottom= y;
+
+        float left2= x - l2;
+        float top2= y - l2 - (l2 / 2);
+        float right2= x;
+        float bottom2= y - (l2 / 2);
+
+        path.arcTo(new RectF(left, top, right, bottom), 90, 180);
+        path.arcTo(new RectF(left2,top2 , right2,bottom2 ), 180, 180);
         path.lineTo(x, y);
         path.close();
 
@@ -71,23 +87,63 @@ public class HeartProgressBar extends View {
         return paint;
     }
 
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        float length = canvas.getWidth()/2 ;
-        float x = canvas.getWidth()/2;
-        float y = canvas.getHeight()/2;
+        float mDensity = getResources().getDisplayMetrics().density;
+        float centerX = (float) w/2;
+        float centerY = (float) h/2;
 
-        canvas.drawPath(firstPart(canvas,length,x,y), firstPaint());
+        rectf = new Rect();
+        getLocalVisibleRect(rectf);
+
+        canvas.drawPath(firstPart(canvas, centerX, centerY), firstPaint());
 
         FloatPoint[] pointArray = getPoints();
 
         for (int i = 0 ;i < progress; i++) {
-            canvas.drawRect(pointArray[i].getX(), pointArray[i].getY(), pointArray[i].getX()+(int) secondRoundWidth,pointArray[i].getY()+(int) secondRoundWidth, secondPaint());
+            canvas.drawCircle(pointArray[i].getX(), pointArray[i].getY(), (int)secondRoundWidth*mDensity , secondPaint());
             invalidate();
         }
 
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        this.w = w;
+        this.h = h;
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int desiredWidth = 250;
+        int desiredHeight = 250;
+
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        if (widthMode == MeasureSpec.EXACTLY) {
+            w = widthSize;
+        } else if (widthMode == MeasureSpec.AT_MOST) {
+            w = Math.min(desiredWidth, widthSize);
+        } else {
+            w = desiredWidth;
+        }
+
+        if (heightMode == MeasureSpec.EXACTLY) {
+            h = heightSize;
+        } else if (heightMode == MeasureSpec.AT_MOST) {
+            h = Math.min(desiredHeight, heightSize);
+        } else {
+            h = desiredHeight;
+        }
+        setMeasuredDimension(w, h);
     }
 
     public int getProgress() {
